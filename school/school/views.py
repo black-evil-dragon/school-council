ac9439e3e50122216c44b5e5184e6a3fa7e86391
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2022-12-26 17:26:20 +0300

    edit views

--- school/school/views.py
+++ school/school/views.py
@@ -5,4 +5,4 @@ def error_handler_404(request, exception):
     return render(request, '404.html', status=404)
 
 def error_handler_500(request, *args, **argv):
-    return render(request, '404.html', status=500)
+    return render(request, '500.html', status=500)
